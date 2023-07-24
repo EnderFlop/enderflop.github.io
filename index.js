9562b6dc44d98a36da9d08400178ddd2b20176a7
enderflop <agbarloon@uiowa.edu>
2023-07-24 18:45:06 -0500

    seeing if this works online

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("hello world!")
+  
+  function loadLeagueMatchLight() {
+    fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  loadLeagueMatchLight()
+
 })
