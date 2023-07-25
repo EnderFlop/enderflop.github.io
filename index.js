36455a3d2cf599e52e487de86de13032a841c03f
enderflop <agbarloon@uiowa.edu>
2023-07-24 19:45:28 -0500

    add league game tracker

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   
   function loadLeagueMatchLight() {
-    fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
-    .then(res => res.json())
-    .then(data => console.log(data))
+    let result = false;
+
+    //fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
+    //.then(res => res.json())
+    //.then(data => result = data)
+
+    const image = document.getElementById("league-image")
+    image.src = result ? "./media/won.png" : "./media/lost.png"
   }
 
   loadLeagueMatchLight()
-
 })
