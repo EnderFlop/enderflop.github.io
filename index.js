07c6396bfa4c763cc0af9223606687cbf4eb74cb
enderflop <agbarloon@uiowa.edu>
2023-07-25 16:54:17 -0500

    make windows draggable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const image = document.getElementById("league-image")
-  function loadLeagueMatchLight() {
+  function loadLeagueMatchWidget() {
+    const image = document.getElementById("league-image")
     fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
     .then(res => res.json())
     .then(data => {
@@ -13,5 +13,56 @@ window.addEventListener("DOMContentLoaded", () => {
     })
   }
 
-  loadLeagueMatchLight()
+  function makeWindowsDraggable() {
+    const windows = Array.from(document.getElementsByClassName("window"))
+    console.log(windows)
+    windows.forEach((windw) => makeDraggable(windw))
+  }
+
+  function makeDraggable(windw) {
+    //https://www.w3schools.com/howto/howto_js_draggable.asp >>> saved me hours of work :DD
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const titleBar = windw.getElementsByClassName("title-bar")[0]
+    titleBar.onmousedown = dragMouseDown;
+
+    function dragMouseDown(event) {
+      event.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = event.clientX;
+      pos4 = event.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(event) {
+      event.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - event.clientX;
+      pos2 = pos4 - event.clientY;
+      pos3 = event.clientX;
+      pos4 = event.clientY;
+      // set the element's new position:
+      windw.style.top = (windw.offsetTop - pos2) + "px";
+      windw.style.left = (windw.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
+  function main () {
+    makeWindowsDraggable()
+    loadLeagueMatchWidget()
+  }
+
+  main()
+
+  //change AWS server to work on local files
+  //add minimize/maximize button
+  //add closing windows
+  //add taskbar where you can reopen windows
 })
