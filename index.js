e35a70746e6e890439ce26c03e4c9cb7f15d4880
enderflop <agbarloon@uiowa.edu>
2023-07-24 20:02:16 -0500

    syntax error

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ window.addEventListener("DOMContentLoaded", () => {
     fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
     .then(res => res.json())
     .then(data => {
-      console.log("win last game:" + data);   
-      image.src = result ? "./media/won.png" : "./media/lost.png"
+      console.log("win last game: " + data);   
+      image.src = data ? "./media/won.png" : "./media/lost.png"
     })
     .catch(error => {
       console.log(error)
