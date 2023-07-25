ab4831f3c3e669a11f65a3ef3b73417174dc57d6
enderflop <agbarloon@uiowa.edu>
2023-07-24 20:00:48 -0500

    improved league widget logic

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
-  
+  const image = document.getElementById("league-image")
   function loadLeagueMatchLight() {
-    let result = true;
-
     fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
     .then(res => res.json())
-    .then(data => {console.log(data); result = data})
-
-    const image = document.getElementById("league-image")
-    image.src = result ? "./media/won.png" : "./media/lost.png"
+    .then(data => {
+      console.log("win last game:" + data);   
+      image.src = result ? "./media/won.png" : "./media/lost.png"
+    })
+    .catch(error => {
+      console.log(error)
+      image.src = "./media/poro.png"
+    })
   }
 
   loadLeagueMatchLight()
