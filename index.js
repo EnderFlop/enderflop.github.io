2fb6c3e27a6a3e2d8c7b4ddccaf7c24ca28869e8
enderflop <agbarloon@uiowa.edu>
2023-07-24 19:46:36 -0500

    change fetch logic

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
   
   function loadLeagueMatchLight() {
-    let result = false;
+    let result = true;
 
-    //fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
-    //.then(res => res.json())
-    //.then(data => result = data)
+    fetch("https://uryoxbdq7g.execute-api.us-east-2.amazonaws.com/", {method:'GET', headers:{'Content-Type': 'application/json'}})
+    .then(res => res.json())
+    .then(data => {console.log(data); result = data})
 
     const image = document.getElementById("league-image")
     image.src = result ? "./media/won.png" : "./media/lost.png"
